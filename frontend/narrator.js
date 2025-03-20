b31b762770e37a4c5dd73f46771aebb4e8ccb92c
Rhys O'Kane <SunburntRock89@gmail.com>
2025-03-20 22:23:10 +0000

    more sounds

--- frontend/narrator.js
+++ frontend/narrator.js
@@ -1,5 +1,8 @@
+const audioCount = 5;
+
 function playFreeFlagsAudio() {
-    const audio = document.getElementById("free-flags-audio");
+    const audio = document.querySelector("#free-flags-audio");
+    audio.src = `sounds/freeflags${Math.round(Math.random() * audioCount) + 1}.mp3`;
     audio.play();
 }
 
