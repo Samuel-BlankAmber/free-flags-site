50583c88883a7289d162e1a9d4c6fdea96e06acf
BlankAmber <145449058+BlankAmber@users.noreply.github.com>
2025-03-21 23:12:13 +0000

    Don't play the same audio twice sequentially

--- frontend/narrator.js
+++ frontend/narrator.js
@@ -1,9 +1,21 @@
-const audioCount = 5;
+const audioCount = 6;
+let lastPlayedIndex = -1;
 
 function playFreeFlagsAudio() {
     const audio = document.getElementById("free-flags-audio");
-    audio.src = `sounds/freeflags${Math.round(Math.random() * audioCount) + 1}.mp3`;
     audio.play();
+    audio.removeEventListener("ended", changeAudioSrc);
+    audio.addEventListener("ended", changeAudioSrc);
+}
+
+function changeAudioSrc() {
+    const audio = document.getElementById("free-flags-audio");
+    let newIndex;
+    do {
+        newIndex = Math.floor(Math.random() * (audioCount - 1)) + 1;
+    } while (newIndex === lastPlayedIndex);
+    lastPlayedIndex = newIndex;
+    audio.src = `sounds/freeflags${newIndex}.mp3`;
 }
 
 document.addEventListener("mousedown", playFreeFlagsAudio);
