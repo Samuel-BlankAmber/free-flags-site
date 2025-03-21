d5a8360a47bbbdf7c32f3abffa071b9c74ba982c
BlankAmber <145449058+BlankAmber@users.noreply.github.com>
2025-03-21 23:03:24 +0000

    Revert war crime

--- frontend/narrator.js
+++ frontend/narrator.js
@@ -1,7 +1,7 @@
 const audioCount = 5;
 
 function playFreeFlagsAudio() {
-    const audio = document.querySelector("#free-flags-audio");
+    const audio = document.getElementById("free-flags-audio");
     audio.src = `sounds/freeflags${Math.round(Math.random() * audioCount) + 1}.mp3`;
     audio.play();
 }
